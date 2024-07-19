fc0d6654ab2781b97245d616a10e8a1f57fd7337
anonymous-63 <anonymous63.krutik@gmail.com>
2024-07-19 21:36:29 +0530

    first commit

---
+++ src/main/java/com/anonymous63/VehicalServiceSystem/VehicleServiceSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anonymous63.VehicalServiceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleServiceSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anonymous63/VehicalServiceSystem/VehicleServiceSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonymous63.VehicalServiceSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
